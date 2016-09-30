e6861d22de3f9507dd8f48eb84dc4fd59c903fc0
Spencer Rinehart <anubis@overthemonkey.com>
2016-09-29 22:11:00 -0500

    Update eslint and fix linting errors.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('assert-dotenv')({}, function() {
-  'use strict';
+require("assert-dotenv")({}, function() {
+  "use strict";
 
-  var Slack = require('slack-node');
-  var Npm = require('npm-publish-stream');
-  var NpmSlack = require('./src/npm-slack');
-  require('datejs');
+  var Slack = require("slack-node");
+  var Npm = require("npm-publish-stream");
+  var NpmSlack = require("./src/npm-slack");
+  require("datejs");
 
   var slack = new Slack();
   slack.setWebHook(process.env.SLACK_WEBHOOK_URL);
@@ -16,10 +16,10 @@ require('assert-dotenv')({}, function() {
 
   var npm = new Npm({startTime: startTime, refreshRate: refreshRate});
 
-  var npmSlack = new NpmSlack({
+  new NpmSlack({
     npm: npm,
     slack: slack,
-    npmPackages: process.env.NPM_PACKAGES.split(','),
+    npmPackages: process.env.NPM_PACKAGES.split(","),
     slackParams: {channel: process.env.SLACK_CHANNEL}
   });
 });
--- src/npm-slack.js
+++ src/npm-slack.js
@@ -1,53 +1,54 @@
-(function() {
-  'use strict';
+"use strict";
 
-  var _ = require('lodash');
+var _ = require("lodash");
 
-  function format(pkg) {
-    var name = pkg.id;
-    var version = pkg.doc['dist-tags'].latest;
-    var link = 'https://www.npmjs.org/package/' + name;
+function format(pkg) {
+  var name = pkg.id;
+  var version = pkg.doc["dist-tags"].latest;
+  var link = "https://www.npmjs.org/package/" + name;
 
-    return 'Package <' + link + '|' + name + '@' + version + '> published';
-  };
+  return "Package <" + link + "|" + name + "@" + version + "> published";
+}
 
-  var NpmSlack = function(opts) {
-    this._npm = opts.npm;
-    this._slack = opts.slack;
-    this._npmPackages = opts.npmPackages;
-    this._slackParams = opts.slackParams;
-    this._format = opts.format || format;
+var NpmSlack = function(opts) {
+  this._npm = opts.npm;
+  this._slack = opts.slack;
+  this._npmPackages = opts.npmPackages;
+  this._slackParams = opts.slackParams;
+  this._format = opts.format || format;
 
-    this._addNpmHandlers();
-  };
+  this._addNpmHandlers();
+};
 
-  NpmSlack.prototype._addNpmHandlers = function() {
-    this._npm.on('error', console.error);
-    this._npm.on('data', this._handleNpmPackage.bind(this));
-  };
+NpmSlack.prototype._addNpmHandlers = function() {
+  // eslint-disable-next-line no-console
+  this._npm.on("error", console.error);
+  this._npm.on("data", this._handleNpmPackage.bind(this));
+};
 
-  NpmSlack.prototype._handleNpmPackage = function(pkg) {
-    if (!_.contains(this._npmPackages, pkg.id)) {
-      return;
-    }
+NpmSlack.prototype._handleNpmPackage = function(pkg) {
+  if (!_.contains(this._npmPackages, pkg.id)) {
+    return;
+  }
 
-    this._sendToSlack(this._format(pkg));
-  };
+  this._sendToSlack(this._format(pkg));
+};
 
-  NpmSlack.prototype._sendToSlack = function(text) {
-    console.log(text);
+NpmSlack.prototype._sendToSlack = function(text) {
+  // eslint-disable-next-line no-console
+  console.log(text);
 
-    this._slack.webhook(
-      _.extend({}, this._slackParams, {text: text}),
-      this._handleSlackResponse.bind(this)
-    );
-  };
+  this._slack.webhook(
+    _.extend({}, this._slackParams, {text: text}),
+    this._handleSlackResponse.bind(this)
+  );
+};
 
-  NpmSlack.prototype._handleSlackResponse = function(error, response) {
-    if (error !== null) {
-      console.error('-- ' + error);
-    }
-  };
+NpmSlack.prototype._handleSlackResponse = function(error) {
+  if (error !== null) {
+    // eslint-disable-next-line no-console
+    console.error("-- " + error);
+  }
+};
 
-  module.exports = NpmSlack;
-})();
+module.exports = NpmSlack;
